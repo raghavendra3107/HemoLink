36d2ff0f3bf97c22f24d96d11ef760d782dc1da9
raghavendra3107 <raghavendrasiva.npt@gmail.com>
2026-02-27 14:47:50 +0530

    fixed cors issue

--- backend/server.js
+++ backend/server.js
@@ -13,10 +13,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors({
-  origin: ["https://hemolink-frontend-eight.vercel.app", "http://localhost:5173"],
-  credentials: true,
-}));
+app.use(cors());
 
 app.use('/api/doc', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
